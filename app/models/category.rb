7228cf380653b7f54b8e89c847d2fe7a7f34dc9d
Pavel Mihailovskiy <pavel.mihailovskiy@gmail.com>
2013-08-03 15:00:12 +0300

    Improved pagination for categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,8 @@ class Category < ActiveRecord::Base
   include Tire::Model::Search
   include Tire::Model::Callbacks
 
+  default_scope order('name ASC')
+
   has_many :assortments
 
   attr_accessible :name
