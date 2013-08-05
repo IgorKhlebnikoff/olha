a019e9e6d9091f8f902c23bbdd4549eb9ad702f2
oleg <ctsit@ukr.net>
2013-08-05 18:39:33 +0300

    Add show profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,11 @@
 class ProfilesController < ApplicationController
-  skip_before_filter :check_profile, only: [:edit, :update]
+  skip_before_filter :check_profile, only: [:show, :edit, :update]
   before_filter :find_profile
+
+  def show
+    @code = Country.find_country_by_alpha2(@profile.country).country_code
+    @country = Carmen::Country.coded(@profile.country).name
+  end
 
   def edit
   end
@@ -8,7 +13,7 @@ class ProfilesController < ApplicationController
   def update
     @profile.update_attributes(params[:profile])
     if @profile.save
-      redirect_to edit_profile_path, notice: 'Profile was successfully updated.'
+      redirect_to @profile, notice: 'Profile was successfully updated.'
     else
       render :edit
     end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -6,6 +6,7 @@ class Profile < ActiveRecord::Base
   validates :first_name, :last_name, presence: true,  length: { minimum: 2 }
   validates :phone_number, presence: true, numericality: true,  length: { minimum: 9 }
   validates :gender, inclusion: { in: %w(Male Female) }  
+  validates :country, presence: true
 
   def full_name
     [first_name, last_name].compact.join(' ')
